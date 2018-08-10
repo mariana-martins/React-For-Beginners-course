40a2455049cfaed19ddda9653643d3c666dfa9b0
mariana-martins <mariana_martins@id.uff.br>
2018-08-10 17:12:00 +1200

    Persisting our State with Firebase

--- src/components/Order.js
+++ src/components/Order.js
@@ -3,18 +3,22 @@ import { formatPrice } from "../helpers";
 
 class Order extends React.Component {
   renderOrder = key => {
-      const fish = this.props.fishes[key];
-      const count = this.props.order[key];
-      const isAvailable = fish.status === 'available';
-      if (!isAvailable) {
-          return <li key={key}>Sorry {fish ? fish.name : 'fish' } is no longer available.</li>
-      }
+    const fish = this.props.fishes[key];
+    const count = this.props.order[key];
+    const isAvailable = fish.status === "available";
+    if (!isAvailable) {
       return (
-          <li key={key}>
-              {count} lbs {fish.name}
-              {formatPrice(count * fish.price)}
-          </li>
+        <li key={key}>
+          Sorry {fish ? fish.name : "fish"} is no longer available.
+        </li>
       );
+    }
+    return (
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    );
   };
   render() {
     const orderIds = Object.keys(this.props.order);
